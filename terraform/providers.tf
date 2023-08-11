7de15ffd02fbdca45395d3673ec18ef8d37aa287
shtajiryan <shtajiryan@hotmail.com>
2023-08-11 15:04:53 +0400

    removed unused ansible provider

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -4,10 +4,7 @@ terraform {
       source  = "hashicorp/aws"
       version = "5.11.0"
     }
-    ansible = {
-      source = "ansible/ansible"
-      version = "1.1.0"
-    }
+
     eksctl = {
       source = "mumoshu/eksctl"
       version = "0.17.0"
